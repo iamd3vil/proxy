d85dbe54cb160b30306e2b63db5ef9e8220c06d8
Sarat Chandra <me@saratchandra.in>
2019-11-21 16:31:56 +0530

    Fine tune TLS configuration

--- proxy.go
+++ proxy.go
@@ -34,7 +34,19 @@ func NewProxy(config Config) (Proxy, error) {
 		}
 
 		tc := &tls.Config{
-			Certificates: []tls.Certificate{cer},
+			Certificates:             []tls.Certificate{cer},
+			PreferServerCipherSuites: true,
+			MinVersion:               tls.VersionTLS12,
+			MaxVersion:               tls.VersionTLS13,
+			CipherSuites: []uint16{
+				tls.TLS_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			},
+			SessionTicketsDisabled: true,
 		}
 		return NewTLSProxy(config.Source, config.Destination, tc)
 	default:
